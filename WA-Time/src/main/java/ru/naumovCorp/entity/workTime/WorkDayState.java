2b1274d38d79c910564ada752d79f437edef075e
Naumov Oleg <o.i.naumov@yandex.ru>
2015-03-21 22:20:12 +0300

    + возможные состояния рабочего дня

---
+++ WA-Time/src/main/java/ru/naumovCorp/entity/workTime/WorkDayState.java
@@ -0,0 +1,30 @@
+package ru.naumovCorp.entity.workTime;
+
+/**
+ * @author Naumov Oleg on 21.03.2015 22:06.
+ */
+
+public enum WorkDayState {
+
+    NO_WORK  ("Не работал", 1),
+    WORKING  ("На работе", 2),
+    COMEAWAY ("Отлучился", 3),
+    WORKED   ("Отработал", 4);
+
+    private String desc;
+    private int order;
+
+    private WorkDayState(String desc, int order) {
+         this.desc = desc;
+         this.order = order;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+}
