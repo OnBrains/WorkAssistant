320e0fc045ac97cdd3ba7ec57a8a76ea2b2a951b
Naumov Oleg <o.i.naumov@yandex.ru>
2015-03-22 16:41:26 +0300

    + DAO интерфейс и его реализация для сущности рабочего дня

---
+++ WA-Time/src/main/java/ru/naumovCorp/dao/workTime/WorkTimeDAOInterface.java
@@ -0,0 +1,20 @@
+package ru.naumovCorp.dao.workTime;
+
+import ru.naumovCorp.entity.workTime.WorkTime;
+import ru.naumovCorp.entity.worker.Worker;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Naumov Oleg on 22.03.2015 14:41.
+ */
+
+public interface WorkTimeDAOInterface {
+
+    public void create(WorkTime workTime);
+    public void update(WorkTime workTime);
+    public void remove(WorkTime workTime);
+    public List<WorkTime> getTimeInfoByMonth(Date month, Worker currentWorker);
+
+}
---
+++ WA-Time/src/main/java/ru/naumovCorp/dao/workTime/implementation/WorkTimeDAO.java
@@ -0,0 +1,50 @@
+package ru.naumovCorp.dao.workTime.implementation;
+
+import ru.naumovCorp.dao.DAOHelper;
+import ru.naumovCorp.dao.workTime.WorkTimeDAOInterface;
+import ru.naumovCorp.entity.workTime.WorkTime;
+import ru.naumovCorp.entity.worker.Worker;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Naumov Oleg on 22.03.2015 14:47.
+ */
+
+@Stateless
+@TransactionManagement(TransactionManagementType.BEAN)
+public class WorkTimeDAO implements WorkTimeDAOInterface {
+
+    @Inject
+    private DAOHelper dh;
+
+    @Override
+    public void create(WorkTime workTime) {
+        dh.persist(workTime);
+    }
+
+    @Override
+    public void update(WorkTime workTime) {
+        dh.merge(workTime);
+    }
+
+    @Override
+    public void remove(WorkTime workTime) {
+        dh.remove(workTime);
+    }
+
+    @Override
+    public List<WorkTime> getTimeInfoByMonth(Date month, Worker currentWorker) {
+        EntityManager em = dh.getEntityManager();
+        currentWorker = em.find(Worker.class, 1L);
+        return em.createNamedQuery(WorkTime.GET_TIME_INFO_BY_MONTH, WorkTime.class)
+                .setParameter("worker", currentWorker).setParameter("day", month).getResultList();
+    }
+
+}
