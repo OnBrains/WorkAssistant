21a884ed82095998614b925a4ac4646f76919e7d
Naumov Oleg <o.i.naumov@yandex.ru>
2015-04-05 01:19:27 +0300

    добавил метов toString, для вывода имени в UI

--- WA-System/src/main/java/ru/naumovCorp/entity/worker/Worker.java
+++ WA-System/src/main/java/ru/naumovCorp/entity/worker/Worker.java
@@ -72,6 +72,11 @@ public class Worker implements Serializable {
     }
 
     @Override
+    public String toString() {
+        return family + " " + firstName;
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
