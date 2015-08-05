7502d614e9a0beba2eba05035c18b6e4249d4a57
HAYMbl4 <o.i.naumov@yandex.ru>
2015-08-05 23:08:37 +0300

    Вынес все основные методы CRUD в DAOHelper

--- WA-System/src/main/java/org/onbrains/dao/DAOHelper.java
+++ WA-System/src/main/java/org/onbrains/dao/DAOHelper.java
@@ -1,6 +1,8 @@
 package org.onbrains.dao;
 
 import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
@@ -9,31 +11,40 @@ import javax.persistence.Persistence;
  */
 
 @Stateless
+@TransactionManagement(TransactionManagementType.BEAN)
 public class DAOHelper {
 
     public static EntityManager getEntityManager() {
         return Persistence.createEntityManagerFactory("WA").createEntityManager();
     }
 
-    public static void persist(Object object) {
+    public void persist(Object object) {
         EntityManager em = getEntityManager();
         em.getTransaction().begin();
         em.persist(object);
         em.getTransaction().commit();
     }
 
-    public static void merge(Object object) {
+    public void merge(Object object) {
         EntityManager em = getEntityManager();
         em.getTransaction().begin();
         em.merge(object);
         em.getTransaction().commit();
     }
 
-    public static void remove(Object object) {
+    public void remove(Object object) {
         EntityManager em = getEntityManager();
         em.getTransaction().begin();
         em.remove(object);
         em.getTransaction().commit();
+    }
+
+    public Object find(Class<?> clazz, Long id) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        Object findObject = em.find(clazz, id);
+        em.getTransaction().commit();
+        return findObject;
     }
 
 }
