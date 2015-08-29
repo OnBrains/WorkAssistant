151ac394087abca3b8fa2c50989e4bb52420507e
HAYMbl4 <o.i.naumov@yandex.ru>
2015-08-29 11:08:22 +0300

    Возможность конвертировать из String'га любой персистентный объект.

--- WA-System/src/main/java/org/onbrains/entity/SuperClass.java
+++ WA-System/src/main/java/org/onbrains/entity/SuperClass.java
@@ -21,6 +21,11 @@ public class SuperClass implements Serializable {
     }
 
     @Override
+    public String toString() {
+        return getClass().getName() + "-" + this.getId();
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
---
+++ WA-web/src/main/java/org/onbrains/viewModel/AbstractPersistentEntityConverter.java
@@ -0,0 +1,58 @@
+package org.onbrains.viewModel;
+
+import org.onbrains.dao.DAOHelper;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Naumov Oleg on 23.08.2015 19:05.
+ */
+@SessionScoped
+@FacesConverter("org.onbrains.viewModel.ObjectConverter")
+public class AbstractPersistentEntityConverter implements Converter {
+
+    private static final Pattern SERIALIZED_FORMAT = Pattern.compile("(.*)\\-([0-9]+)");
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        if (value == null || "".equals(value)) {
+            return null;
+        }
+
+        Matcher matcher = SERIALIZED_FORMAT.matcher(value);
+        if (!matcher.matches()) {
+            throw new ConverterException(new FacesMessage("'" + value + "' неверное значение, необходимо выбрать значение из предложеных"));
+        }
+
+        Class<?> entityClass = null;
+        Long primaryKey = null;
+        try {
+            entityClass = getClass(matcher.group(1));
+            primaryKey = getId(matcher.group(2));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return DAOHelper.getEntityManager().find(entityClass, primaryKey);
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        return value.toString();
+    }
+
+    private Class<?> getClass(String classPath) throws ClassNotFoundException {
+        return Class.forName(classPath);
+    }
+
+    private Long getId(String idValue) {
+        return Long.parseLong(idValue);
+    }
+}
